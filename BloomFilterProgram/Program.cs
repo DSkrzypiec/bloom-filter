08ba0e7663a68cfdd466ab7b2ca3ef789852a8cb
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2021-09-12 17:22:54 +0200

    Project setup

---
+++ BloomFilterProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BloomFilterLib;
+
+namespace BloomFilterProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bf = new BloomFilter();
+            Console.WriteLine("Bloom Filter - to be implemented.");
+        }
+    }
+}
